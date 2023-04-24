b402a18e7d84b5795731b9d0c8d67f537f199975
JVandenildo <jvandenildo@gmail.com>
2023-04-24 19:06:58 -0300

    Largest Palindrome Product (not done)

---
+++ Largest Palindrome Product/LargestPalindromeProduct.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int LargestPalindrome(int digits){
+    // no idea how to do it
+
+    return digits;
+}
+
+int main(){
+    std::cout << "How many digits? ";
+    int digits;
+    std::cin >> digits;
+    std::cout << LargestPalindrome(digits) << ".\n";
+
+    return 0;
+}

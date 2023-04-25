910721e769cd6e837fbe87f270ff0a0d62947377
JVandenildo <jvandenildo@gmail.com>
2023-04-25 10:51:40 -0300

    smallest multiple

---
+++ Smallest Multiple/SmallestMultiple.py
@@ -0,0 +1,17 @@
+def SmallestMultiple(minimum, maximum):
+    number = maximum
+    init = minimum
+
+    while init <= maximum:
+        if number % init == 0:
+            init = init + 1
+        else:
+            number = number + 1
+            init = minimum
+    
+    return number
+
+minimum = int(input("Minimum: "))
+maximum = int(input("Maximum: "))
+
+print(SmallestMultiple(minimum, maximum))

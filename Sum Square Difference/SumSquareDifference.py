1f3936abb58878662d1d206eb35c5819c54c4fa8
JVandenildo <jvandenildo@gmail.com>
2023-04-25 17:59:25 -0300

    Sum Square Difference

---
+++ Sum Square Difference/SumSquareDifference.py
@@ -0,0 +1,20 @@
+def SumSquareDifference(firstRange, secondRange):
+    init = 1
+    sumFirstRange = 0
+    while init <= firstRange:
+        sumFirstRange = sumFirstRange + (init*init)
+        init = init + 1
+    
+    init = 1
+    sumSecondRange = 0
+    while init <= secondRange:
+        sumSecondRange = sumSecondRange + init
+        init = init + 1
+    sumSecondRange = sumSecondRange * sumSecondRange
+    
+    return sumSecondRange - sumFirstRange
+
+firstRange = int(input("First range: "))
+secondRange = int(input("Second range: "))
+
+print(SumSquareDifference(firstRange, secondRange))

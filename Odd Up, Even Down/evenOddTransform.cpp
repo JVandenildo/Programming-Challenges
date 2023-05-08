a27418def6e93926a6865e15289a7b83e376170c
JVandenildo <jvandenildo@gmail.com>
2023-05-08 07:21:07 -0300

    even_odd transformation

---
+++ Odd Up, Even Down/evenOddTransform.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> evenOddTransform(int n){
+    vector<int> numberList = {3, 4, 9};
+    int count = 0;
+
+    while(count < n){
+        for(int i = 0; i <= numberList.size(); i++){
+            if(numberList[i] % 2 == 0){
+                numberList[i] = numberList[i] - 2;
+            }
+            else{
+                numberList[i] = numberList[i] + 2;
+            }
+        }
+        count = count + 1;
+    }
+
+    return numberList;
+}
+
+int main(){
+    int n;
+    cout << "Number of transformations: ";
+    cin >> n;
+
+    for(auto i: evenOddTransform(n)){
+        cout << i << ", ";
+    }
+    cout << "Size of "<< evenOddTransform(n).size() << ".\n";
+
+    return 0;
+}
